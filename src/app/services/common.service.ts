88b7dda295ab4bfabccb8cbd8a6ac0c19e0fcf10
Mohd Jisan <mjisan@neuvays.com>
2024-09-05 02:05:57 +0530

    updated

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CommonService {
-  apiUrl: any  = "https://3e39-203-92-37-218.ngrok-free.app/";
+  apiUrl: any  = "https://ffe3-203-92-37-218.ngrok-free.app/";
 
   constructor(private http: HttpClient) { }
   serviceForm(data: any) {
@@ -17,6 +17,9 @@ export class CommonService {
   }
   contactUsForm(data: any) {
   return this.http.post(this.apiUrl + `contactUs`, data);
+}
+careerForm(data: any) {
+  return this.http.post(this.apiUrl + `career`, data);  
 }
 
 }
